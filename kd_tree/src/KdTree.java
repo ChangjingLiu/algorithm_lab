eb9858c954cadaf48a9c8e95cc7b20a18ba138dd
LIUCJ <593562829@qq.com>
2021-10-26 00:21:18 +0800

    个人电脑提交 2021/10/26 0:20

--- kd_tree/src/KdTree.java
+++ kd_tree/src/KdTree.java
@@ -15,11 +15,12 @@ public class KdTree {
         private int depth;
         //private boolean vert;
 
-        public Node(Point2D p, int depth) {
+        public Node(Point2D p, int depth, RectHV rect) {
             if (p == null)
                 throw new NullPointerException();
             this.p = p;
             //this.N = N;
+            this.rect = rect;
             this.depth = depth;
         }
     }
@@ -38,27 +39,32 @@ public class KdTree {
     }
 
     public void insert(Point2D p) {
-        root = insert(root, p, 0);
+        RectHV Rect = new RectHV(0, 0, 1, 1);
+        root = insert(root, p, 0, Rect);
     }
 
-    private Node insert(Node x, Point2D p, int depth) {
+    private Node insert(Node x, Point2D p, int depth, RectHV rect) {
         if (x == null) {
-            return new Node(p, depth);//其他节点
+            return new Node(p, depth, rect);//其他节点
         } else {
             //left,right
-            if (depth % 2 != 0) {
+            if (depth % 2 == 0) {
                 if (p.x() < x.p.x()) {
-                    x.lb = insert(x.lb, p, depth + 1);
+                    RectHV leftRect = new RectHV(rect.xmin(), rect.ymin(), x.p.x(), rect.ymax());
+                    x.lb = insert(x.lb, p, depth + 1, leftRect);
                 } else {
-                    x.rt = insert(x.rt, p, depth + 1);
+                    RectHV rightRect = new RectHV(x.p.x(), rect.ymin(), rect.xmax(), rect.ymax());
+                    x.rt = insert(x.rt, p, depth + 1, rightRect);
                 }
             }
             //top,bottom
             else {
                 if (p.y() < x.p.y()) {
-                    x.lb = insert(x.lb, p, depth + 1);
+                    RectHV bottomRect = new RectHV(rect.xmin(), rect.ymin(), rect.xmax(), x.p.y());
+                    x.lb = insert(x.lb, p, depth + 1, bottomRect);
                 } else {
-                    x.rt = insert(x.rt, p, depth + 1);
+                    RectHV topRect = new RectHV(rect.xmin(), x.p.y(), rect.xmax(), rect.ymax());
+                    x.rt = insert(x.rt, p, depth + 1, topRect);
                 }
             }
         }
@@ -66,22 +72,28 @@ public class KdTree {
     }
 
     public void draw() {
-        for (Node node : Allnode()) {
-            node.p.draw();
-            if (node.depth % 2 != 0) {
+        for (Node node : Get_Nodes()) {
+
+
+            if (node.depth % 2 == 0) {
                 //vertical
+                StdDraw.setPenRadius(0.005);
                 StdDraw.setPenColor(StdDraw.RED);
-                StdDraw.line(node.p.x(), 0, node.p.x(), 1);
+                StdDraw.line(node.p.x(), node.rect.ymin(), node.p.x(), node.rect.ymax());
             } else {
                 //h
-                StdDraw.setPenColor(StdDraw.RED);
-                StdDraw.line(0, node.p.y(), 1, node.p.y());
+                StdDraw.setPenRadius(0.005);
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.line(node.rect.xmin(), node.p.y(), node.rect.xmax(), node.p.y());
             }
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.setPenRadius(0.01);
+            node.p.draw();
         }
         StdDraw.show();
     }
 
-    public Iterable<Node> Allnode() {
+    public Iterable<Node> Get_Nodes() {
         Queue<Node> kNodes = new Queue<Node>();
         order(root, kNodes);
         return kNodes;
--- kd_tree/src/KdTreeVisualizer.java
+++ kd_tree/src/KdTreeVisualizer.java
@@ -19,10 +19,10 @@ public class KdTreeVisualizer {
         RectHV rect = new RectHV(0.0, 0.0, 1.0, 1.0);
         StdDraw.enableDoubleBuffering();
         KdTree kdtree = new KdTree();
-        while (true) {
-            if (StdDraw.isMousePressed()) {
-                double x = StdDraw.mouseX();
-                double y = StdDraw.mouseY();
+        for (int i = 0; i < 6; i++) {
+            if (true) {
+                double x = 0.1 + 0.1 * (double) i;
+                double y = 0.1 + 0.15 * (double) i;
                 StdOut.printf("%8.6f %8.6f\n", x, y);
                 Point2D p = new Point2D(x, y);
                 if (rect.contains(p)) {

b8c9e0df4869f2c081dce073f58a5584f145d6aa
CJ LIU <593562829@qq.com>
2021-11-13 22:31:45 +0800

    实验室 2021/11/13 22:31

--- SeamCarver/src/SeamCarver.java
+++ SeamCarver/src/SeamCarver.java
@@ -25,7 +25,16 @@ public class SeamCarver {
     public int height() {
         return picture.height();
     }
-    
+
+    // energy of pixel at column x and row y
+    public double energy(int x, int y) {
+        if (!(x >= 0 && x <= this.picture.width() - 1)) {
+            throw new IllegalArgumentException();
+        }
+        if (!(y >= 0 && y <= this.picture.height() - 1)) {
+            throw new IllegalArgumentException();
+        }
+    }
 
     //  unit testing (optional)
     public static void main(String[] args) {

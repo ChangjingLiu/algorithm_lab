7da5f8c6683e277df2b78e0f15b7212b90e85742
LIUCJ <593562829@qq.com>
2021-11-15 23:54:27 +0800

    pc 2021/11/15 23:54

--- SeamCarver/src/SeamCarver.java
+++ SeamCarver/src/SeamCarver.java
@@ -1,6 +1,6 @@
-package src;
-
 import edu.princeton.cs.algs4.Picture;
+
+import java.awt.*;
 
 public class SeamCarver {
 
@@ -33,6 +33,30 @@ public class SeamCarver {
         }
         if (!(y >= 0 && y <= this.picture.height() - 1)) {
             throw new IllegalArgumentException();
+        }
+        if(x==0||x==this.picture.width() - 1||y==0||y==this.picture.height()-1){
+            return 1000;
+        }
+        else{
+            //x-gradient
+            int R_x= (this.picture.getRGB(x-1,y)>> 16)&0xFF-
+                    (this.picture.getRGB(x+1,y)>> 16)&0xFF;
+            int G_x= (this.picture.getRGB(x-1,y)>> 8)&0xFF-
+                    (this.picture.getRGB(x+1,y)>> 8)&0xFF;
+            int B_x= (this.picture.getRGB(x - 1, y))&0xFF-
+                    (this.picture.getRGB(x + 1, y))&0xFF;
+            int RGB_x=R_x*R_x+G_x*G_x+B_x*B_x;
+
+            //Y-gradient
+            int R_y= (this.picture.getRGB(x,y-1)>> 16)&0xFF-
+                    (this.picture.getRGB(x,y+1)>> 16)&0xFF;
+            int G_y= (this.picture.getRGB(x,y-1)>> 8)&0xFF-
+                    (this.picture.getRGB(x,y+1)>> 8)&0xFF;
+            int B_y= (this.picture.getRGB(x, y-1))&0xFF-
+                    (this.picture.getRGB(x, y+1))&0xFF;
+            int RGB_y=R_y*R_y+G_y*G_y+B_y*B_y;
+
+            return java.lang.Math.sqrt(RGB_x+RGB_y);
         }
     }
 

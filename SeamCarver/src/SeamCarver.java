f8801062850f9cf319cd4033f1b28a8ef7515afb
CJ LIU <593562829@qq.com>
2021-11-16 19:30:05 +0800

    实验室 2021/11/14 19:30

--- SeamCarver/src/SeamCarver.java
+++ SeamCarver/src/SeamCarver.java
@@ -1,5 +1,4 @@
 import edu.princeton.cs.algs4.Picture;
-import edu.princeton.cs.algs4.Stack;
 
 public class SeamCarver {
 
@@ -8,6 +7,9 @@ public class SeamCarver {
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
+        if (picture == null) {
+            throw new IllegalArgumentException();
+        }
         this.picture = picture;
         computeenergy();
     }
@@ -48,17 +50,15 @@ public class SeamCarver {
         if (x == 0 || x == width() - 1 || y == 0 || y == height() - 1) {
             return 1000;
         } else {
-            //x-gradient
+            // x-gradient
             int left = this.picture.getRGB(x - 1, y);
             int right = this.picture.getRGB(x + 1, y);
             int R_x = ((left >> 16) & 0xFF) - ((right >> 16) & 0xFF);
             int G_x = ((left >> 8) & 0xFF) - ((right >> 8) & 0xFF);
             int B_x = ((left) & 0xFF) - ((right) & 0xFF);
             int RGB_x = R_x * R_x + G_x * G_x + B_x * B_x;
-            //int a = (left >> 0) & 0xFF;
-            //int b = (right >> 0) & 0xFF;
 
-            //Y-gradient
+            // Y-gradient
             int top = this.picture.getRGB(x, y + 1);
             int bottom = this.picture.getRGB(x, y - 1);
             int R_y = ((top >> 16) & 0xFF) - ((bottom >> 16) & 0xFF);
@@ -66,7 +66,7 @@ public class SeamCarver {
             int B_y = ((top) & 0xFF) - ((bottom) & 0xFF);
             int RGB_y = R_y * R_y + G_y * G_y + B_y * B_y;
 
-            return java.lang.Math.sqrt(RGB_x + RGB_y);
+            return Math.sqrt(RGB_x + RGB_y);
         }
     }
 
@@ -82,39 +82,29 @@ public class SeamCarver {
             }
 
         }
-        //因为是拓扑排序，先遍历0，0 0，1
         for (int row = 0; row < height() - 1; row++) {
             for (int col = 0; col < width(); col++) {
-                if (col == 3 && row == 2) {
-                    int a = 0;
-                }
                 relax(col - 1, row + 1, col, row, disto, pathto);
                 relax(col, row + 1, col, row, disto, pathto);
                 relax(col + 1, row + 1, col, row, disto, pathto);
             }
 
         }
-        //find min mindisto
+        // find min mindisto
         int min_disto = -1;
         double min_energy = Double.MAX_VALUE;
 
-        //遍历最后一行
         for (int col = 0; col < width(); col++) {
             if (disto[col][height() - 1] < min_energy) {
                 min_energy = disto[col][height() - 1];
                 min_disto = col;
             }
         }
-        //find the path
-        Stack<Integer> stack = new Stack<>();
-        for (int i = height() - 1; i >= 0; i--) {
-            stack.push(min_disto);
-            min_disto = pathto[min_disto][i];
-        }
+        // find the path
         int[] rs = new int[height()];
-        int i = 0;
-        for (int n : stack) {
-            rs[i++] = n;
+        for (int i = height() - 1; i >= 0; i--) {
+            rs[i] = min_disto;
+            min_disto = pathto[min_disto][i];
         }
         return rs;
     }
@@ -128,7 +118,7 @@ public class SeamCarver {
         return res;
     }
 
-    public void trans(Picture picture) {
+    private void trans(Picture picture) {
         int newheight = width();
         int newwidth = height();
         Picture newpicture = new Picture(newwidth, newheight);
@@ -144,9 +134,9 @@ public class SeamCarver {
 
     private void relax(int col, int row, int lastcol, int lastrow, double disto[][], int pathto[][]) {
         if ((col >= 0 && col <= width() - 1 && row >= 0 && row <= height() - 1)) {
-            //out_of_bound
+            // out_of_bound
             double w = energy[col][row];
-            //double a = disto[col][row];
+            // double a = disto[col][row];
             if (disto[col][row] > disto[lastcol][lastrow] + w) {
 
                 //double b = disto[lastcol][row - 1] + w;
@@ -158,17 +148,22 @@ public class SeamCarver {
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
+        if (seam == null) {
+            throw new IllegalArgumentException();
+        }
         if (seam.length != height()) {
             throw new IllegalArgumentException();
         }
         if (width() <= 1) {
             throw new IllegalArgumentException();
         }
-        if (seam == null) {
-            throw new IllegalArgumentException();
+        for (int k : seam) {
+            if (!(k >= 0 && k <= width() - 1)) {
+                throw new IllegalArgumentException();
+            }
+
         }
         Picture newpicture = new Picture(width() - 1, height());
-        int del_row = 0;
         for (int j = 0; j < height(); j++) {
             int ii = 0;
             for (int i = 0; i < width() - 1; i++) {
@@ -194,8 +189,6 @@ public class SeamCarver {
 
     //  unit testing (optional)
     public static void main(String[] args) {
-
+     
     }
-
-
 }

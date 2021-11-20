ef688c4f468ecd680207d8e6b454d42c6bc9215b
CJ LIU <593562829@qq.com>
2021-11-20 12:17:48 +0800

    实验室 2021/11/20 12:17

--- BaseballElimination/src/BaseballElimination.java
+++ BaseballElimination/src/BaseballElimination.java
@@ -1,3 +1,9 @@
 public class BaseballElimination {
-    
+    public BaseballElimination(String filename) {
+        
+    }
+
+    public static void main(String[] args) {
+        BaseballElimination division = new BaseballElimination(args[0]);
+    }
 }

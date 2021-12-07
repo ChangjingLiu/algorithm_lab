d4be5da5b2a9cd75891dd1fd0081f3083d14f93f
LIUCJ <593562829@qq.com>
2021-12-07 15:03:43 +0800

    个人PC 2021/12/07 15:03

--- BoggleSolver/src/BoggleSolver.java
+++ BoggleSolver/src/BoggleSolver.java
@@ -18,72 +18,73 @@ public class BoggleSolver {
     private BoggleBoard board;
     private Node root;
 
-    public BoggleSolver(String[] dictionary){
+    public BoggleSolver(String[] dictionary) {
         root = new Node();
         int cnt = 0;
-        for(String s:dictionary){
+        for (String s : dictionary) {
             put(s, cnt);
             //dic.put(s,cnt);
             cnt++;
         }
 //        StdOut.print(dic.contains("AI"));
     }
-    private static class Node{
+
+    private static class Node {
         private int val;
         private final Node[] next = new Node[26];
     }
 
-    private int get(String key){
-        Node x= get(root, key, 0);
-        if(x==null) return 0;
+    private int get(String key) {
+        Node x = get(root, key, 0);
+        if (x == null) return 0;
         return x.val;
     }
 
-    private Node get(Node x,String key,int d){
-        if(x==null) return null;
-        if(d==key.length()) return x;
-        int c = key.charAt(d)-'A';
-        return get(x.next[c], key, d+1);
+    private Node get(Node x, String key, int d) {
+        if (x == null) return null;
+        if (d == key.length()) return x;
+        int c = key.charAt(d) - 'A';
+        return get(x.next[c], key, d + 1);
     }
 
-    private void put(String key,int val){
-        root=put(root, key, val, 0);
+    private void put(String key, int val) {
+        root = put(root, key, val, 0);
     }
 
-    private Node put(Node x, String key,int val,int d){
-        if(x == null) x = new Node();
-        if(d == key.length()){
+    private Node put(Node x, String key, int val, int d) {
+        if (x == null) x = new Node();
+        if (d == key.length()) {
             x.val = 1;
             return x;
         }
-        int c=key.charAt(d)-'A';
-        x.next[c] = put(x.next[c],key,val,d+1);
+        int c = key.charAt(d) - 'A';
+        x.next[c] = put(x.next[c], key, val, d + 1);
         return x;
     }
 
-    private boolean isExist(int i,int j){
+    private boolean isExist(int i, int j) {
         return i >= 0 && i < rows && j >= 0 && j < cols;
     }
 
-    private void CreatAdj(BoggleBoard board){
-        this.board=board;
-        cols= this.board.cols();
-        rows= this.board.rows();
-        adj= new Bag[cols*rows];//建立bag，长度为dice个数
+    private void CreatAdj(BoggleBoard board) {
+        this.board = board;
+        cols = this.board.cols();
+        rows = this.board.rows();
+        adj = new Bag[cols * rows];//建立bag，长度为dice个数
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                int v=i*cols+j;//当前dice的索引
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int v = i * cols + j;//当前dice的索引
                 adj[v] = new Bag<Integer>();//为每个dice建立一个bag
                 //八个方向
-                if(isExist(i-1,j-1))  adj[v].add((i-1)*cols+(j-1));//left_top
-                if(isExist(i-1,j))  adj[v].add((i-1)*cols+(j));//top
-                if(isExist(i-1,j+1))  adj[v].add((i-1)*cols+(j+1));//right_top
-                if(isExist(i,j+1))  adj[v].add((i)*cols+(j+1));//right
-                if(isExist(i+1,j+1))  adj[v].add((i+1)*cols+(j+1));//right_bottom
-                if(isExist(i+1,j))  adj[v].add((i+1)*cols+(j));//bottom
-                if(isExist(i+1,j-1))  adj[v].add((i+1)*cols+(j-1));//left_bottom
-                if(isExist(i,j-1))  adj[v].add((i)*cols+(j-1));//left
+                if (isExist(i - 1, j - 1)) adj[v].add((i - 1) * cols + (j - 1));//left_top
+                if (isExist(i - 1, j)) adj[v].add((i - 1) * cols + (j));//top
+                if (isExist(i - 1, j + 1)) adj[v].add((i - 1) * cols + (j + 1));//right_top
+                if (isExist(i, j + 1)) adj[v].add((i) * cols + (j + 1));//right
+                if (isExist(i + 1, j + 1)) adj[v].add((i + 1) * cols + (j + 1));//right_bottom
+                if (isExist(i + 1, j)) adj[v].add((i + 1) * cols + (j));//bottom
+                if (isExist(i + 1, j - 1)) adj[v].add((i + 1) * cols + (j - 1));//left_bottom
+                if (isExist(i, j - 1)) adj[v].add((i) * cols + (j - 1));//left
             }
         }
 
@@ -101,54 +102,54 @@ public class BoggleSolver {
 
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
-    public Iterable<String> getAllValidWords(BoggleBoard board){
-        validwords=new SET<String>();
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        validwords = new SET<String>();
 
         CreatAdj(board);
 
-        for(int i=0;i< adj.length;i++){
-            marked=new boolean[adj.length];
+        for (int i = 0; i < adj.length; i++) {
+            marked = new boolean[adj.length];
             Stack<Integer> visitingDices = new Stack<Integer>();
 
             visitingDices.push(i);
-            marked[i]=true;
-            char c=getLetterOnBoard(i);
-            if(c=='Q') {
-                dfs(i,root.next['Q'-'A'].next['U'-'A'],"QU", visitingDices);
-            }else{
-                dfs(i,root.next[c-'A'],c+"", visitingDices);
+            marked[i] = true;
+            char c = getLetterOnBoard(i);
+            if (c == 'Q') {
+                dfs(i, root.next['Q' - 'A'].next['U' - 'A'], "QU", visitingDices);
+            } else {
+                dfs(i, root.next[c - 'A'], c + "", visitingDices);
             }
         }
 
         return validwords;
     }
 
-    private void dfs(Integer v, Node x, String str, Stack<Integer> visitingDices){
+    private void dfs(Integer v, Node x, String str, Stack<Integer> visitingDices) {
 
-        if(str.length()>=3&&x!=null){
-            if(x.val==1){
+        if (str.length() >= 3 && x != null) {
+            if (x.val == 1) {
                 validwords.add(str);
             }
         }
 
 
-        for(Integer s:adj[v]){
+        for (Integer s : adj[v]) {
             char c = getLetterOnBoard(s);
             //Queue<String> tmp= (Queue<String>) dic.keysWithPrefix(str+c);
-            if(!marked[s] &&x!=null){
-                if(x.next[c - 'A'] != null){
+            if (!marked[s] && x != null) {
+                if (x.next[c - 'A'] != null) {
                     visitingDices.push(s);
-                    marked[s]=true;
+                    marked[s] = true;
 
-                    if(c=='Q'){
-                        dfs(s,x.next['Q'-'A'].next['U'-'A'],str+"QU",visitingDices);
-                    }else{
-                        dfs(s,x.next[c-'A'],str+c,visitingDices);
+                    if (c == 'Q') {
+                        dfs(s, x.next['Q' - 'A'].next['U' - 'A'], str + "QU", visitingDices);
+                    } else {
+                        dfs(s, x.next[c - 'A'], str + c, visitingDices);
                     }
 
 
                     int index = visitingDices.pop();
-                    marked[index]=false;
+                    marked[index] = false;
                 }
             }
 
@@ -164,26 +165,22 @@ public class BoggleSolver {
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
-    public int scoreOf(String word){
-        if(get(word)==0){
+    public int scoreOf(String word) {
+        if (get(word) == 0) {
             return 0;
-        }
-        else{
-            int len=word.length();
-            if(len<3){
+        } else {
+            int len = word.length();
+            if (len < 3) {
                 return 0;
-            }
-            else if(len<=4){
+            } else if (len <= 4) {
                 return 1;
-            }
-            else if(len==5){
+            } else if (len == 5) {
                 return 2;
-            }
-            else if(len==6){
+            } else if (len == 6) {
                 return 3;
-            }else if(len==7){
+            } else if (len == 7) {
                 return 5;
-            }else{
+            } else {
                 return 11;
             }
         }

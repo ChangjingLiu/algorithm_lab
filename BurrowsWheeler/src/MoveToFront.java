d20dbd913bf55468897960b4a6402ec03c028e5b
LIUCJ <593562829@qq.com>
2021-12-09 13:54:47 +0800

    pc 2021/12/09 13:54

--- BurrowsWheeler/src/MoveToFront.java
+++ BurrowsWheeler/src/MoveToFront.java
@@ -1,2 +1,10 @@
 public class MoveToFront {
+    // apply move-to-front encoding, reading from standard input and writing to standard output
+    public static void encode() {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
